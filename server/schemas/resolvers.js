d3e969612527d0666bafb2754bd772f7e8fe99f9
patrickcorcoran10 <patrickcorcoran10@gmail.com>
2022-03-19 11:12:47 -0500

    commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,61 @@
+const { Book, User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+// User:
+// Query: getSingleUser
+// Mutation: createUser
+// TBD: login
+// Book:
+// Mutation: saveBook, deleteBook (this updates the User)
+
+const resolvers = {
+  Query: {
+    me: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return User.find(params);
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { user, body }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: body } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+    removeBook: async (parent, { user, params }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
